1d5a9f395ea5e7699f83988ce2290a372f2371fa
kelllllllly <kellyveintimilla1@gmail.com>
2025-01-29 19:09:44 -0500

    cpu info

--- main.cc
+++ main.cc
@@ -8,12 +8,20 @@ int main() {
 fstream thisProcFile; 
 
 // Open the file for CPU information fstream thisProcFile; 
-thisProcFile.open("nproc", ios::in);
+thisProcFile.open("/proc/sys/kernel/hostname", ios::in);
 // Read the file. Note this file only has the host name in it
 string line; 
 getline(thisProcFile, line); 
 cout << "Hostname: " << line << endl << endl; 
 
+// getting the number of processers 
+cout << "Number of processsing units: ";
+while (getline(thisProcFile,line)){
+
+    if (line.find("processor") != string::npos){
+        cout << line << endl;
+    }
+}
 thisProcFile.close();
 
 return 0;
