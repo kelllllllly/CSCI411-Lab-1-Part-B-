a784344b3380a1330d59d17b12b159a40ba3bf9e
kelllllllly <kellyveintimilla1@gmail.com>
2025-01-29 19:49:24 -0500

    testing soemthign..

--- main.cc
+++ main.cc
@@ -21,14 +21,21 @@ thisProcFile.close();
 // getting the number of processers 
 thisProcFile.open("/proc/cpuinfo", ios::in);
 int count = 0; 
-string readin;
-while (getline(thisProcFile,readin)){
-    if (readin.find("processor") != string::npos){
+//string readin;
+while (getline(thisProcFile,line)){
+    if (line.find("processor") != string::npos){
         count++;
     }
 }
 cout << "Number of processsing units: " << count << endl;
 thisProcFile.close();
+
+// getting the CPU type and model 
+//thisProcFile.open("cat /proc/cpuinfo", ios::in); 
+//while(getline(thisProcFile, line)){
+ //   if(line,)
+//}
+//thisProcFile.close();
 
 return 0;
 }
